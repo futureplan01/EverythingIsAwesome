e55ad65388fa3a89abaf84d905543bc2b1ffb9d0
Venoth <venothx7@hotmail.com>
2019-02-09 15:19:20 -0500

    Switching different branch to see if my VSC works

--- Components/Welcome.js
+++ Components/Welcome.js
@@ -1,25 +1,12 @@
 // User Clicks a button and it navigates to the welcome screen
 
 import React from 'react';
-import {Text} from 'react-native';
+import {
+  Text
+} from 'react-native';
 
-// lets make it a function 
-
-export default function Welcome(props){
-    return <Text style ={styles.welcome}> This is the welcome page </Text>;
+export default function Welcome(props) {
+  return <Text style = {
+    Styles.welcome
+  } > This is the welcome page < /Text>;
 }
-
-const styles = StyleSheet.create({
-    welcome: {
-      fontSize: 20,
-      textAlign: 'center',
-      margin: 10,
-    },
-    instructions: {
-      textAlign: 'center',
-      color: '#333333',
-      marginBottom: 5,
-    },
-  });
-
-// I want to style it 
