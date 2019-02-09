8d394c3cd398183f2aa7b733c116bf2a58f74546
Venoth <venothx7@hotmail.com>
2019-02-09 15:29:15 -0500

    created a single Style component

--- Components/Welcome.js
+++ Components/Welcome.js
@@ -1,25 +1,11 @@
 // User Clicks a button and it navigates to the welcome screen
 
-import React from 'react';
-import {Text} from 'react-native';
+import React from "react";
+import { Text } from "react-native";
+import Style from "./Style";
 
-// lets make it a function 
+// lets make it a function
 
-export default function Welcome(props){
-    return <Text style ={styles.welcome}> This is the welcome page </Text>;
+export default function Welcome(props) {
+  return <Text style={Style.welcome}> This is the welcome page </Text>;
 }
-
-const styles = StyleSheet.create({
-    welcome: {
-      fontSize: 20,
-      textAlign: 'center',
-      margin: 10,
-    },
-    instructions: {
-      textAlign: 'center',
-      color: '#333333',
-      marginBottom: 5,
-    },
-  });
-
-// I want to style it 
