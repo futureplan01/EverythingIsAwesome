e4b91eadfc7aa6f70ada5b4095c161d4add5c260
Venoth <venothx7@hotmail.com>
2019-02-12 16:43:51 -0500

    changed configurations to make camera work

--- Components/Camera.js
+++ Components/Camera.js
@@ -1,8 +1,16 @@
-import Camera from 'react-native-camera';
+import {
+    RNCamera,
+    FaceDector
+} from 'react-native-camera';
 import React from 'react';
 import Style from "./Style";
-import {Text} from "react-native";
+import {
+    Text
+} from "react-native";
+
 
 export default function Welcome(props) {
-    return <Text style={Style.welcome}> This is the camera page </Text>;
+    return <Text style = {
+        Style.welcome
+    } > This is the camera page < /Text>;
 }
