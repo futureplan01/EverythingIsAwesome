87b7f670c6d46f1d07cf6ddef1d3560cf66b0742
Venoth <venothx7@hotmail.com>
2019-02-11 21:28:24 -0500

    Navigation Working

--- Components/Welcome.js
+++ Components/Welcome.js
@@ -1,11 +1,24 @@
 // User Clicks a button and it navigates to the welcome screen
 
 import React from "react";
-import { Text } from "react-native";
+import { Text, View,Button } from "react-native";
 import Style from "./Style";
 
 // lets make it a function
 
-export default function Welcome(props) {
+export default class Welcome extends React.Component {
+  render() {
+    return (
+       <View>
+        <Text style={Style.welcome}> This is the welcome page </Text>
+        <Button title = "Click Here For Camera"
+          onPress = {()=> this.props.navigation.navigate('Camera')}
+        />
+      </View>);
+  }
+}
+
+
+function Welcome(props) {
   return <Text style={Style.welcome}> This is the welcome page </Text>;
 }
