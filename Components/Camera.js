ac1ba1bcaf81d282e7ee6367a35903efe1ea7ed1
Venoth <venothx7@hotmail.com>
2019-02-13 07:16:34 -0500

    integrated a camera with my app

--- Components/Camera.js
+++ Components/Camera.js
@@ -1,16 +1,70 @@
-import {
-    RNCamera,
-    FaceDector
-} from 'react-native-camera';
-import React from 'react';
+import { RNCamera, FaceDector } from "react-native-camera";
+import React from "react";
 import Style from "./Style";
 import {
-    Text
+  AppRegistry,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View
 } from "react-native";
 
-
-export default function Welcome(props) {
-    return <Text style = {
-        Style.welcome
-    } > This is the camera page < /Text>;
+export default class Camera extends React.Component {
+  render() {
+    return (
+      <View>
+        <RNCamera
+          ref={ref => {
+            this.camera = ref;
+          }}
+          type={RNCamera.Constants.Type.back}
+          flashMode={RNCamera.Constants.FlashMode.on}
+          permissionDialogTitle={"Permission to use camera"}
+          permissionDialogMessage={"We need your permission"}
+          onGoogleVisionBarcodesDetected={({ barcode }) => {
+            console.log(barcode);
+          }}
+        />
+        <View
+          style={{ flex: 0, flexDirection: "row", justifyContent: "center" }}
+        >
+          <TouchableOpacity
+            onPress={this.takePicture.bind(this)}
+            style={styles.capture}
+          >
+            <Text style={{ fontSize: 14 }}> SNAP </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  }
+  takePicture = async function() {
+    if (this.camera) {
+      const options = { quality: 0.5, base64: true };
+      const data = await this.camera.takePictureAsync(options);
+      console.log(data.uri);
+    }
+  };
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "column",
+    backgroundColor: "black"
+  },
+  preview: {
+    flex: 1,
+    justifyContent: "flex-end",
+    alignItems: "center"
+  },
+  capture: {
+    flex: 0,
+    backgroundColor: "#fff",
+    borderRadius: 5,
+    padding: 15,
+    paddingHorizontal: 20,
+    alignSelf: "center",
+    margin: 20
+  }
+});
