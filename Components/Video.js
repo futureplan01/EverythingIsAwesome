5b96bd88fc707376aee925ed8565c9d29f96400f
Venoth <venothx7@hotmail.com>
2019-02-20 19:24:36 -0500

    setting up keyStore to publish to play store

---
+++ Components/Video.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import {View, StyleSheet} from 'react-native';
+import Video from 'react-native-video'
+
+export dedfualt class VideoComponent extends React.Component{
+    renderVideo (){
+        return (
+            <Video
+                so
+            />
+        )
+    }
+}
